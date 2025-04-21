8cf4cb6a736464148bc8a8d6e99192b14d099fb9
Proglov <dev.mohammad.parvizi@gmail.com>
2025-04-21 21:30:51 +0330

    feat: added two global pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -10,24 +10,20 @@ export default function Error({
     reset: () => void
 }) {
     return (
-        <html dir='rtl'>
-            <body>
-                <div className='w-full text-center relative' style={{ height: '100vh' }}>
-                    <div className='w-full absolute top-1/3'>
-                        <p className='m-3 text-red-600'>متاسفم ارور دادم رفت :(</p>
-                        <div className='flex flex-col justify-center'>
-                            <Button variant='ghost' onClick={() => reset()} className='text-indigo-500 flex justify-center gap-1 text-base'>
-                                یبار دیگه امتحان کن خب
-                                <RotateCcw />
-                            </Button>
-                            <Link href="/" className='text-emerald-500 flex justify-center gap-1'>
-                                اگه نشد برگرد به خونه اول
-                                <House className='text-xs w-4' />
-                            </Link>
-                        </div>
-                    </div>
+        <div className='w-full text-center relative' style={{ height: '100vh' }}>
+            <div className='w-full absolute top-1/3'>
+                <p className='m-3 text-red-600'>متاسفم ارور دادم رفت :(</p>
+                <div className='flex flex-col justify-center'>
+                    <Button variant='ghost' onClick={() => reset()} className='text-indigo-500 flex justify-center gap-1 text-base'>
+                        یبار دیگه امتحان کن خب
+                        <RotateCcw />
+                    </Button>
+                    <Link href="/" className='text-emerald-500 flex justify-center gap-1'>
+                        اگه نشد برگرد به خونه اول
+                        <House className='text-xs w-4' />
+                    </Link>
                 </div>
-            </body>
-        </html>
+            </div>
+        </div>
     )
 }
