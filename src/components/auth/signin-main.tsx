858941e22591700b9136a5375c341007d8ab0cad
Proglov <dev.mohammad.parvizi@gmail.com>
2025-04-25 13:45:57 +0330

    feat: added framer motion

--- src/components/auth/signin-main.tsx
+++ src/components/auth/signin-main.tsx
@@ -2,16 +2,26 @@
 import { useSearchParams } from "next/navigation"
 import SigninPhone from "./signin-phone";
 import SigninUsername from "./signin-username";
+import { AnimatePresence, motion } from "framer-motion";
 
 const USERNAME_TAB = 'username';
 
-
 export default function SigninMain() {
     const searchParams = useSearchParams()
-
     const tab = searchParams.get('tab');
+    let content = tab === USERNAME_TAB ? <SigninUsername /> : <SigninPhone />;
 
-    if (tab === USERNAME_TAB)
-        return <SigninUsername />;
-    return <SigninPhone />;
+    return (
+        <AnimatePresence mode="wait" >
+            <motion.div
+                key={tab}
+                style={{ overflow: 'hidden' }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -20, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+            >
+                {content}
+            </motion.div>
+        </AnimatePresence>
+    )
 }
