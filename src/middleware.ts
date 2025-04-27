6287cfbda8279fbe828b9bc6f73b9ae49157e7fa
Proglov <dev.mohammad.parvizi@gmail.com>
2025-04-27 17:43:10 +0330

    feat(middleware): added middleware to check if the user is admin

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+
+//check the admin jwt
+export async function middleware(request: NextRequest) {
+    const cookieHeader = request.headers.get('cookie') || '';
+
+    try {
+        if (!cookieHeader.includes('Authentication=')) return NextResponse.redirect(new URL('/not-found', request.url));
+
+        const response = await fetch('http://localhost:4500/users/admin', { headers: { Cookie: cookieHeader } });
+        const isAdmin = await response.json();
+        if (!isAdmin) return NextResponse.redirect(new URL('/not-found', request.url));
+
+        return NextResponse.next();
+    } catch (error) {
+        return NextResponse.redirect(new URL('/not-found', request.url));
+    }
+}
+
+
+export const config = {
+    matcher: ['/admin/:path*'],
+};
