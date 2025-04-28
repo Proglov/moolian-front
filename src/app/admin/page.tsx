d33629d748c0f83070df91d05fc66a0922aa59fc
Proglov <dev.mohammad.parvizi@gmail.com>
2025-04-28 16:40:24 +0330

    ui

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,21 @@
 import Products from '@/components/admin/Products'
 import Users from '@/components/admin/Users';
+import Button from '@/components/shared/Button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Undo } from 'lucide-react';
+import Link from 'next/link';
 
 
 
 export default async function page() {
-
     return (
         <div className='m-5'>
+            <Button asChild variant='secondary' className='text-destructive'>
+                <Link href={'/'}>
+                    <Undo />
+                    بازگشت
+                </Link>
+            </Button>
             <Tabs defaultValue="products" className='max-w-5xl mx-auto'>
                 <TabsList dir='rtl'>
                     <TabsTrigger value="products">محصولات</TabsTrigger>
