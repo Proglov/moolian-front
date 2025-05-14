9f522692715644fcdf6c99940f277f6c97a44d0d
Proglov <dev.mohammad.parvizi@gmail.com>
2025-05-14 16:29:43 +0330

    feat(menu): added link to home

--- src/components/home/Menu.tsx
+++ src/components/home/Menu.tsx
@@ -9,12 +9,18 @@ import {
 } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
 import { sidebarData } from "@/lib/sidebar-data";
+import Link from "next/link";
 
 
 export default function NavigationMenuWithDropdown() {
   return (
     <NavigationMenu className="z-40 hidden md:block" dir="rtl">
       <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href='/' className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-primary text-black px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1">
+            خانه
+          </Link>
+        </NavigationMenuItem>
         {
           sidebarData.map(item => (
             <NavigationMenuItem key={item.title}>
