f8d7bf1caf034f312363dae47d20a38fa1e11daa
Proglov <dev.mohammad.parvizi@gmail.com>
2025-05-29 07:50:03 +0330

    check if next config is getting deployed

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,77 @@
 import type { NextConfig } from "next";
 
+console.log('NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNEEEEEEEEEEEEEEEEEEEEEEEEEXXXXXXXXXXXXXXXXXXXXXXTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT');
+
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '3000',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.c2.liara.space',
+        pathname: '/moolian-image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'moolian-image.storage.c2.liara.space',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'shopiing.storage.iran.liara.space',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.iran.liara.space',
+        pathname: '/**',
+      },
     ],
   },
 };
 
 export default {
-  ...nextConfig
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+      {
+        source: '/firebase-messaging-sw.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'",
+          },
+        ],
+      },
+    ]
+  }
 };
